fdbb7bc5e9ffb20827f35546ec5a251a810a39fa
Tom Mount <tmountjr@gmail.com>
2022-09-13 12:02:56 -0400

    start on default layout with navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-navbar toggleable="lg" variant="dark" type="dark">
+    <b-navbar-brand to="/">
+      Webauthn Demo
+    </b-navbar-brand>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item to="/logout" right>
+        Sign Out
+      </b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent'
+}
+</script>
